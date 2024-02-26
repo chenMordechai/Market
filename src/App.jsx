252ccdcb97a187889f80bbbb6b72af2839da0741
Chen <chen100030@gmail.com>
2024-02-26 14:12:35 +0200

    add Login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+
 import './assets/style/main.css'
 import { Index } from './pages/Index'
+import { Login } from './pages/Login'
 
 export function App() {
   return (
-    <section>
-      <Index />
-    </section>
+    <Router>
+      <section>
+        <Routes>
+          <Route element={<Index />} path="/" />
+          <Route element={<Login />} path="/login" />
+
+        </Routes>
+      </section>
+    </Router>
   )
 }
 
---
+++ src/pages/Login.jsx
@@ -0,0 +1,9 @@
+
+
+export function Login() {
+    return (
+        <section className="login">
+            <h1>Login</h1>
+        </section>
+    )
+}
